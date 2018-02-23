33b0ceeef670c27299a12b57febb1700e005b1d3
Utkarsh <utkarsh.sahu@proptiger.com>
2018-02-23 09:39:55 +0530

    added splash pages

---
+++ public/javascripts/splash.js
@@ -0,0 +1,11 @@
+checkUserOffers = (uid) => {
+  
+window.location.href = "http://"+window.location.href.split('/')[2]+'/sellerdashboard?uid='+uid;
+
+}
+
+claimOwnerListing = (uid) => {
+
+//window.location.href = "http://"+window.location.href.split('/')[2]+'/owner?uid='+uid;
+
+}
